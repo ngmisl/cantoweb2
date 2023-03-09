5d290ecf416fab7617636aaaffc545f7a80e07f3
chris <ngmi@posteo.es>
2023-03-09 12:31:18 +0100

    2023.03.09-12:31:17

--- src/components/mint/App.jsx
+++ src/components/mint/App.jsx
@@ -2,7 +2,6 @@ import { ConnectKitButton } from 'connectkit';
 import React from 'react';
 import { MintNFT } from './Minter';
 import { useAccount } from 'wagmi'
-import TotalSupply from './Totalmints';
 
 export function App() {
   const { isConnected } = useAccount()
@@ -11,7 +10,6 @@ export function App() {
       <div>
         <div><ConnectKitButton/> </div>
         <div><MintNFT /></div>
-        <div><TotalSupply /></div>
       </div>
     )
   }
