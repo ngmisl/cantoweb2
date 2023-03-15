8e7ffedfd5c4aa2b6643df0d353fa25f6c624d9b
chris <ngmi@posteo.es>
2023-03-15 11:12:22 +0100

    updates

--- src/components/mint/NewMint.jsx
+++ src/components/mint/NewMint.jsx
@@ -1,6 +1,6 @@
-import { Contract, providers, utils } from "ethers";
+import { Contract, providers } from "ethers";
 import React, { useEffect, useRef, useState } from "react";
-import Web3Modal, { local } from "web3modal";
+import Web3Modal from "web3modal";
 import { abi, NFT_CONTRACT_ADDRESS } from "./constants";
 import styles from "./styles/Home.module.css";
 
