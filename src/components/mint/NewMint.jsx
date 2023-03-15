b1c17e812b4d337bf30a256192aab28a9337ecb0
chris <ngmi@posteo.es>
2023-03-15 13:29:14 +0100

    2023.03.15-13:29:13

--- src/components/mint/NewMint.jsx
+++ src/components/mint/NewMint.jsx
@@ -95,9 +95,9 @@ export default function Home() {
 
     // If user is not connected to the Mumbai network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 7701) {
-      window.alert("Change the network to Cantoverse Testnet");
-      throw new Error("Change network to Cantoverse Testnet");
+    if (chainId !== 7700) {
+      window.alert("Change the network to Cantoverse");
+      throw new Error("Change network to Cantoverse");
     }
 
     if (needSigner) {
